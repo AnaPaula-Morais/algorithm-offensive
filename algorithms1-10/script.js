0a98e086ee7a7696dcaacd3a66fc055737ef78d3
Ana Paula <ap.almeidapm@gmail.com>
2024-05-28 14:19:27 +0100

    criacao do repositorio git

---
+++ algorithms1-10/script.js
@@ -0,0 +1,10 @@
+//001 - print the message "You have to do algorithms every day to learn!".
+function algorithm1(){
+    var commitment = "You have to do algorithms every day to learn!";
+    alert(commitment);
+}
+//002 - type your name.
+function algorithm2(){
+    var name = prompt("Type your name");
+    alert("Olá " + name);
+}
