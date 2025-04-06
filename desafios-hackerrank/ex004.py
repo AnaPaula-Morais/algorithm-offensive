c8dc431e8bb235adacd0b00910571ba38158266b
Ana Paula <ap.almeidapm@gmail.com>
2025-04-06 17:50:01 +0100

    Correcao do codigo sobre ano bissexto

--- desafios-hackerrank/ex004.py
+++ desafios-hackerrank/ex004.py
@@ -13,12 +13,16 @@ Note que o stub de código fornecido lê de STDIN e passa argumentos para a is_l
 def is_leap(year):
     leap = False
     
-    if (year % 4 == 0 and year % 400 == 0 and year % 100 == 0 ):
-        print(True)
-    elif(year % 4 == 0 and year % 400 == 0 and year % 100 != 0):
-        print(False)
-    
-    return leap
+    if(year % 4 == 0):
+        if(year % 100 == 0):
+            if(year % 400 == 0):
+                return True
+            else:
+                return False
+        else: 
+            return True
+    else:
+        return False
 
 year = int(input("Digite um ano, para saber se é bissexto ou não: "))
 print(is_leap(year))
